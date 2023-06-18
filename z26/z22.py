53e9cadcf53a79f7560febcabb890569aa54ccaa
Stanislav Shishkin <shishkinstas@mail.ru>
2023-06-18 15:59:08 +0700

    first commit

--- z26/z22.py
+++
@@ -1,9 +0,0 @@
-a = int(input('Введите число: '))
-b = int(input('Введите степень числа: '))
-
-def square(a, b):
-    if b == 0:
-        return 1
-    return a * square(a, b-1)
-
-print(square(a, b))
